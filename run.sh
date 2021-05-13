cc525403808c8030b7bf9690b7b7cbb99ef5709f
Ozlem Yildiz <zy2043@log-3.nyu.cluster>
2021-05-13 00:36:03 -0400

    Update run

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:4
-#SBATCH --time=5:00:00
-#SBATCH --mem=2GB
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --mem=30GB
 #SBATCH --job-name=geese
 #SBATCH --mail-type=END
 #SBATCH --mail-user=zy2043@nyu.edu
 #SBATCH --output=geese%j.out
+
+conda activate geese
 
 cd /scratch/zy2043/Eat-Move-Learn/notebooks/
 
