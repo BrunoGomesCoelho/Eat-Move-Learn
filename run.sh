8441ef20e8e96d61f35fcc6b1aa8f1cd1a20782c
Özlem Yıldız <ozlemyildiz@10-21-251-108.dynapool.wireless.nyu.edu>
2021-05-12 23:22:51 -0400

    To run at HPC

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:4
+#SBATCH --time=5:00:00
+#SBATCH --mem=2GB
+#SBATCH --job-name=geese
+#SBATCH --mail-type=END
+#SBATCH --mail-user=zy2043@nyu.edu
+#SBATCH --output=geese%j.out
+
+cd /scratch/zy2043/Eat-Move-Learn/notebooks/
+
+python geese_dqn_1opponent.py 
+#python geese_dqn_2opponents.py 
+#python geese_dqn_3opponents.py 
+#python geese_dqn_multiopponents.py 
+
+
